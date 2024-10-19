6b0e9a947d1dec4357117ae5a6c48ed556e29a22
jakubwojcik <jakubwojcik.sh@gmail.com>
2024-10-19 18:23:35 +0200

    refactor: update icon paths in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,38 +2,11 @@ import { TanStackRouterVite as tanstackRouter } from "@tanstack/router-plugin/vi
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import { VitePWA as pwa } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   base: process.env.GITHUB_REPOSITORY ? `/fizjo-plus/` : "/",
-  plugins: [
-    react(),
-    tanstackRouter(),
-    svgr(),
-    pwa({
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-      manifest: {
-        description: "",
-        icons: [
-          {
-            sizes: "192x192",
-            src: "src/assets/icons/icon-192x192.png",
-            type: "image/png",
-          },
-          {
-            sizes: "512x512",
-            src: "src/assets/icons/icon-512x512.png",
-            type: "image/png",
-          },
-        ],
-        name: "fizjo-plus",
-        short_name: "fizjo-plus",
-        theme_color: "#ffffff",
-      },
-      registerType: "autoUpdate",
-    }),
-  ],
+  plugins: [react(), tanstackRouter(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
