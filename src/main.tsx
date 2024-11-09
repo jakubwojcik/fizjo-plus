bde393bdd9143b41979922dd7c8682e87882e53a
Jakub Wójcik <jakub.wojcik@caspio.com>
2024-11-09 21:30:29 +0100

    fix: update router configuration to use hash history for routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import "@fontsource/roboto";
 import "./styles/globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import {
+  createHashHistory,
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
@@ -11,7 +15,7 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const router = createRouter({ history: createHashHistory(), routeTree });
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
